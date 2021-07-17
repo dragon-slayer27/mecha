94b9a091e066d189eb1259adc98d8081ba29fb22
vivaan teotia <vivaan.teotia@gmail.com>
2021-07-17 15:30:48 +0530

    added a basic command handler

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+
 const config = require('./config.json')
+const command = require('./command')
 
 client.on('ready', () =>{
     console.log('The Client is Ready!')
+
+    command(client, ['ping', 'test'], message => {
+        message.channel.send('Pong!')
+    })
 })
 
 client.login(config.token)
